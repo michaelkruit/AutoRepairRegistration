82ee707f9de0d55fa3fecdd077cc55bfe283bf21
michaelkruit <michael_mikin@hotmail.com>
2019-01-27 20:25:03 +0100

    Register updates

--- SammysAuto/Data/Migrations/20190127192328_addUserProperties.Designer.cs
+++ SammysAuto/Data/Migrations/20190127192328_addUserProperties.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SammysAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190127192328_addUserProperties")]
+    partial class addUserProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
