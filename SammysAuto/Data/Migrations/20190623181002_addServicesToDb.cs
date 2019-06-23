cd4d7dff45b4b554995596fa560ed4ac96dddb49
michaelkruit <michael_mikin@hotmail.com>
2019-06-23 20:10:49 +0200

    Added services class

---
+++ SammysAuto/Data/Migrations/20190623181002_addServicesToDb.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SammysAuto.Data.Migrations
+{
+    public partial class addServicesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CarId = table.Column<int>(nullable: false),
+                    CardId = table.Column<int>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    Details = table.Column<string>(nullable: true),
+                    Miles = table.Column<double>(nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    ServiceTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Services_Cars_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Services_ServiceTypes_ServiceTypeId",
+                        column: x => x.ServiceTypeId,
+                        principalTable: "ServiceTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_CardId",
+                table: "Services",
+                column: "CardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_ServiceTypeId",
+                table: "Services",
+                column: "ServiceTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Services");
+        }
+    }
+}
